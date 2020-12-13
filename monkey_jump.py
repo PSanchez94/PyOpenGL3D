55246ebd81dca785dab570d42a69b15d90156f97
PSanchez94 <pablo.sanchez.r@ug.uchile.cl>
2020-12-13 17:37:05 -0300

    Added floor plane and three camera views

--- monkey_jump.py
+++ monkey_jump.py
@@ -24,6 +24,25 @@ import controller
 controller = controller.Controller()
 
 
+def createPlane(length, r, g, b):
+
+    # Defining the location and colors of each vertex  of the shape
+    vertices = [
+    #    positions        colors
+        0, 0,  0, r, g, b,
+        length, 0,  0, r, g, b,
+        length,  length,  0, r, g, b,
+        0,  length,  0, r, g, b]
+
+    # Defining connections among vertices
+    # We have a triangle every 3 indices specified
+    indices = [
+        0, 1, 2,
+        2, 3, 0]
+
+    return bs.Shape(vertices, indices)
+
+
 def on_key(window, key, scancode, action, mods):
 
     if action == glfw.PRESS:
@@ -51,8 +70,8 @@ if __name__ == "__main__":
     if not glfw.init():
         sys.exit()
 
-    width = 600
-    height = 600
+    width = 800
+    height = 800
 
     window = glfw.create_window(width, height, "Monkey Jump 3D", None, None)
 
@@ -72,7 +91,7 @@ if __name__ == "__main__":
     glUseProgram(mvpPipeline.shaderProgram)
 
     # Setting up the clear screen color
-    glClearColor(0.85, 0.85, 0.85, 1.0)
+    glClearColor(0.15, 0.15, 0.15, 1.0)
 
     # As we work in 3D, we need to check which part is in front,
     # and which one is at the back
@@ -80,37 +99,77 @@ if __name__ == "__main__":
 
     # Creating shapes on GPU memory
     gpuAxis = es.toGPUShape(bs.createAxis(7))
+    gpuZPlane = es.toGPUShape(createPlane(7, 0.3, 0.9, 0))
 
     # Using the same view and projection matrices in the whole application
-    projection = tr.perspective(45, float(width)/float(height), 0.1, 100)
+    projection = tr.perspective(60, float(width)/float(height), 0.1, 100)
     glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
-    
-    view = tr.lookAt(
-            np.array([5,5,7]),
-            np.array([0,0,0]),
-            np.array([0,0,1])
-        )
-    glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+
+    side_view = np.array([10 * np.cos(np.pi / 4), 10 * np.cos(np.pi / 4), 6])
+    viewPos = side_view
+
+    t0 = glfw.get_time()
 
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
         glfw.poll_events()
 
-        # Clearing the screen in both, color and depth
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # Getting the time difference from the previous iteration
+        t1 = glfw.get_time()
+        dt = t1 - t0
+        t0 = t1
+
+        if glfw.get_key(window, glfw.KEY_B) == glfw.PRESS:
+            viewPos = side_view
+
+        elif glfw.get_key(window, glfw.KEY_N) == glfw.PRESS:
+            viewPos = np.array([0, 10, 0.1])
+
+        elif glfw.get_key(window, glfw.KEY_M) == glfw.PRESS:
+            viewPos = np.array([0, 0.1, 10])
+
+        view = tr.lookAt(
+            viewPos,
+            np.array([0, 0, 0]),
+            np.array([0, 0, 1])
+        )
+
+        glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "view"),
+                           1,
+                           GL_TRUE,
+                           view)
+
+        glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"),
+                           1,
+                           GL_TRUE,
+                           tr.translate(-3.5, 0, 0))
+        mvpPipeline.drawShape(gpuZPlane)
+
+
 
         # Filling or not the shapes depending on the controller state
-        if (controller.fillPolygon):
+        if controller.fillPolygon:
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
         else:
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
 
+        # Clearing the screen in both, color and depth
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
         if controller.showAxis:
-            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"),
+                               1,
+                               GL_TRUE,
+                               tr.identity())
             mvpPipeline.drawShape(gpuAxis, GL_LINES)
+
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"),
+                               1,
+                               GL_TRUE,
+                               tr.translate(-3.5, -3.5, 0))
+            mvpPipeline.drawShape(gpuZPlane)
 
         # Once the render is done, buffers are swapped, showing only the complete scene.
         glfw.swap_buffers(window)
 
-    
     glfw.terminate()
